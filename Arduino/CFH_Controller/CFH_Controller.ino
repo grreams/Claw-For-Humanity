efee28fa38e09c3798ca13eaa5013dc2048d642d
Matvey <45541154+matveho@users.noreply.github.com>
2023-05-14 18:02:58 -0700

    test

--- Arduino/CFH_Controller/CFH_Controller.ino
+++ Arduino/CFH_Controller/CFH_Controller.ino
@@ -1,5 +1,6 @@
-String incomingByte = 0;
-int choice = 0;
+String incomingString;
+String command;
+int argument;
 
 void setup() {
   pinMode(2, OUTPUT);
@@ -14,68 +15,40 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0) {
-    incomingString = Serial.readString();
-    choice = incomingString.toInt();
+    incomingString = Serial.readStringUntil('\n'); // Read the command until newline character
+    parseCommand(incomingString);
   }
+}
+
+void parseCommand(String commandString) {
+  int separatorIndex = commandString.indexOf(':'); // Find the separator character
+
+  if (separatorIndex != -1) { // If separator found
+    command = commandString.substring(0, separatorIndex); // Extract the command
+    argument = commandString.substring(separatorIndex + 1).toInt(); // Extract the argument as an integer
+
+    executeCommand();
+  }
+}
+
+void executeCommand() {
+  if (command == "ON") {
+    turnOnDevice(argument);
+  }
+  else if (command == "OFF") {
+    turnOffDevice(argument);
+  }
+  // Add more command cases as needed
+}
+
+void turnOnDevice(int deviceNumber) {
+  // Perform actions to turn on the specified device
+  digitalWrite(deviceNumber, HIGH);
+}
+
+void turnOffDevice(int deviceNumber) {
+  // Perform actions to turn off the specified device
+  digitalWrite(deviceNumber, LOW);
+}
   
-  switch(choice){
-    case 0:
-      // Drive off
-      digitalWrite(6, HIGH);
-
-      // All pistons off
-      digitalWrite(1, LOW);
-      digitalWrite(2, LOW);
-      digitalWrite(3, LOW);
-      digitalWrite(4, LOW);
-      digitalWrite(5, LOW);
-      break;
-    case 1:
-      // Drive on
-      digitalWrite(6, LOW);
-
-      // All pistons on
-      digitalWrite(1, HIGH);
-      digitalWrite(2, HIGH);
-      digitalWrite(3, HIGH);
-      digitalWrite(4, HIGH);
-      digitalWrite(5, HIGH);
-      break;
-    case 2:
-      // Drive off
-      digitalWrite(6, HIGH);
-      break;
-    case 3:
-      // Drive on
-      digitalWrite(6, LOW);
-      break;
-    case 4:
-      // Piston 1 on
-      digitalWrite(1, HIGH);
-      break;
-    case 5:
-      // Piston 1 off
-      digitalWrite(1, LOW);
-      break;
-    case 6:
-      // Piston 2 on
-      digitalWrite(2, HIGH);
-      break;
-    case 7:
-      // Piston 2 off
-      digitalWrite(2, LOW);
-      break;
-    case 8:
-      // Piston 3, 4 on
-      digitalWrite(3, HIGH);
-      digitalWrite(4, HIGH);
-      break;
-    case 9:
-      // Piston 3, 4 off
-      digitalWrite(3, LOW);
-      digitalWrite(4, LOW);
-      break;
-    default:
-      break;
-  }
 }
